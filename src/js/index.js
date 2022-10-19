6eab10010f851c66bc42ab7e04a090f8ec756ef1
trantuvan <trantuvan.kan@gmail.com>
2022-10-19 10:36:56 +0700

    add link to css

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-import changeFavicon from './utils/changeFavicon';
+// CSS
+import '../assets/css/normalize.css';
+import '../assets/css/reset.css';
+import '../assets/css/style.css';
+
+// IMG
 import Logo from '../assets/img/favicon.png';
+
+// JS
+import changeFavicon from './utils/changeFavicon';
 
 (() => {
   changeFavicon(Logo);
