ab6b3126fef64b4c23854905aed175bbb921e6b8
trantuvan <trantuvan.kan@gmail.com>
2022-10-24 20:05:29 +0700

    form validation for empty string && whitespaces

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 // CSS
 import '../assets/css/normalize.css';
 import '../assets/css/reset.css';
@@ -8,14 +9,35 @@ import Logo from '../assets/img/favicon.png';
 
 // JS
 import changeFavicon from './utils/changeFavicon';
-import handleError from './utils/handlePromiseError';
 import FetchAsync from './utils/fetchAsync';
+import handleError from './utils/handlePromiseError';
 
 (() => {
   changeFavicon(Logo);
-  handleError(FetchAsync.getGeographicPosition)('fffs').then((data) =>
-    // *return an array of locations meet the criteria
-    // *return [] if cannot meet the criteria
-    console.log('data', data)
-  );
+  const searchBox = document.querySelector('#search');
+  const searchBtn = document.querySelector('.search-btn');
+
+  searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const cityName = searchBox.value;
+
+    // *check for empty string and whitespaces
+    if (cityName.trim().length === 0) {
+      const errorMessage = document.querySelector('.form-message');
+      errorMessage.textContent = '* Required properly city name';
+      errorMessage.classList.remove('disabled');
+
+      // *remove error message
+      setTimeout(() => {
+        errorMessage.classList.add('disabled');
+      }, 3000);
+
+      return undefined;
+    }
+
+    handleError(FetchAsync.getGeographicPosition)(cityName).then((data) =>
+      console.log('data', data)
+    );
+  });
 })();
